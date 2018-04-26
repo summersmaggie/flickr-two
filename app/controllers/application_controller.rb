f1e3ead26e0b537815536fae73ed3a5310ba3632
Guest User <Guest@Epicodus-3D.local>
2018-04-26 10:15:52 -0700

    refactor authorization to use helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :is_admin?
+
+  def is_admin?
+    current_user && current_user.admin
+  end
+
 end
